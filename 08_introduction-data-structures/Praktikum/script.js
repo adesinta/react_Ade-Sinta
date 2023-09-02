14db9f013b804ba3dff67935f98e55c94926e00e
Ades <adesintaaaa@gmail.com>
2023-09-02 16:35:38 +0800

    add file praktikum and screenshots 08 Data structure

--- 08_introduction-data-structures/Praktikum/script.js
+++ 08_introduction-data-structures/Praktikum/script.js
@@ -1,38 +1,88 @@
-const productForm = document.getElementById('productForm')
-const productNameInput = document.getElementById('productName')
-const productCategoryInput = document.getElementById('productCategory')
-const additionalDescriptionInput = document.getElementById('additionalDescription')
-const productPriceInput = document.getElementById('productPrice')
+const productForm = document.getElementById("productForm");
+const productNameInput = document.getElementById("productName");
+const productCategoryInput = document.getElementById("productCategory");
+const productFreshnessInputs = document.getElementsByName("flexRadioDefault");
+const additionalDescriptionInput = document.getElementById("additionalDescription");
+const productPriceInput = document.getElementById("productPrice");
 const validateLetter = /^[a-zA-Z]+$/;
+const tableBody = document.getElementById("tableBody");
+const products = [];
 
 productForm.addEventListener('submit', event => {
-    if(productNameInput.value.length > 25 ){
-        event.preventDefault();
-        alert('Product Name must not exceed 25 characters.')
+    event.preventDefault();
+
+    const productName = productNameInput.value;
+    const productCategory = productCategoryInput.value;
+    const productFreshness = getSelectedRadioValue(productFreshnessInputs);
+    const additionalDescription = additionalDescriptionInput.value;
+    const productPrice = productPriceInput.value;
+
+    if (productName.length > 25) {
+        alert("Product Name must not exceed 25 characters.");
+        return;
     }
 
-    if(productNameInput.value.trim() === ''){
-        event.preventDefault();
-        alert('Please enter a valid product name')
-    }  
-
-    if(!validateLetter.test(productNameInput.value)){
-        event.preventDefault();
-        alert('Name must not contain symbols.')
+    if (productName.trim() === "") {
+        alert("Please enter a valid product name.");
+        return;
     }
 
-    if(productPriceInput.value.trim() === ''){
-        event.preventDefault();
-        alert('Please enter a valid Product Price')
+    if (!validateLetter.test(productName)) {
+        alert("Name must not contain symbols.");
+        return;
     }
 
-    if(additionalDescriptionInput.value.trim() === ''){
-        event.preventDefault();
-        alert('Additonal Description must be filled in')
+    if (productPrice.trim() === "") {
+        alert("Please enter a valid Product Price.");
+        return;
     }
 
-    if(productCategoryInput.value === ''){
-        event.preventDefault();
-        alert('Product Category must be filled in')
+    if (additionalDescription.trim() === "") {
+        alert("Additional Description must be filled in.");
+        return;
     }
-});
+
+    if (productCategory === "") {
+        alert("Product Category must be filled in.");
+        return;
+    }
+
+    addProductFormToTable(productName, productCategory, productFreshness, additionalDescription,  productPrice);
+    productForm.reset()
+
+})
+
+const addProductFormToTable = (name, category, freshness, description, price) => {
+    const product = { name, category, freshness, description,  price };
+    products.push(product);
+
+    addDataTable();
+}
+
+const addDataTable = () => {
+    tableBody.innerHTML = "";
+
+    for (let i = 0; i < products.length; i++) {
+        const newRow = tableBody.insertRow(tableBody.rows.length);
+
+        newRow.innerHTML = `
+            <td>${i + 1}</td>
+            <td>${products[i].name}</td>
+            <td>${products[i].category}</td>
+            <td>${products[i].freshness}</td>
+            <td>${products[i].description}</td>
+            <td>${products[i].price}</td>
+        `;
+    }
+}
+
+function getSelectedRadioValue(radioInputs) {
+    for (const radioInput of radioInputs) {
+        if (radioInput.checked) {
+            return radioInput.value;
+        }
+    }
+    return null;
+}
+
+
